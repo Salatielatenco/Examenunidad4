ec5c09d78656cb6d3758aac83ea3b6fc5cfe0238
salatielatenco <carlossalatielatenco@gmail.com>
2021-12-06 16:57:33 -0600

    version2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Examenunidad4';
+  title = 'ExamenUnidad4';
+  articulos : any = null ;
+
+  art ={
+    codigo :null,
+    descripcion: null,
+    precio:null
+  }
+
+  constructor(private articulosServicio: ArticulosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.articulosServicio.recuperarTodos().subscribe(result => this.articulos = result);
+  }
+
+  alta() {
+    this.articulosServicio.alta(this.art).subscribe((datos : any ) => {
+      if (datos['resultado']) {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  baja(codigo : any) {
+    this.articulosServicio.baja(codigo).subscribe((datos : any ) => {
+      if (datos['resultado']=='OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  modificacion() {
+    this.articulosServicio.modificacion(this.art).subscribe((datos : any ) => {
+      if (datos['resultado']=='OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  seleccionar(codigo : any) {
+    this.articulosServicio.seleccionar(codigo).subscribe((result : any ) => this.art = result[0]);
+  }
+
+  hayRegistros() {
+    return true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ArticulosService } from './articulos.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArticulosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
